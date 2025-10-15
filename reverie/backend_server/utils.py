5c20b3c2dc537fc2763732e9921840b6e89e15e7
Hongyiqiao <125090194@link.cuhk.edu.cn>
2025-10-15 17:53:12 +0800

    utils.py

---
+++ reverie/backend_server/utils.py
@@ -0,0 +1,16 @@
+# Copy and paste your OpenAI API Key
+openai_api_key = "<sk-fAQNl78Ge57a90BA449cT3BlBKFJ7361e62a9A204853a5a4>"
+# Put your name
+key_owner = "<hongyiqiao>"
+
+maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
+env_matrix = f"{maze_assets_loc}/the_ville/matrix"
+env_visuals = f"{maze_assets_loc}/the_ville/visuals"
+
+fs_storage = "../../environment/frontend_server/storage"
+fs_temp_storage = "../../environment/frontend_server/temp_storage"
+
+collision_block_id = "32125"
+
+# Verbose 
+debug = True
